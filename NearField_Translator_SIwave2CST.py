5465f7678505071ad00860b7779aa01bed10600b
sogentle <sogentle@me.com>
2018-12-02 18:09:21 +0900

    XML writer without values

--- NearField_Translator_SIwave2CST.py
+++ NearField_Translator_SIwave2CST.py
@@ -5,7 +5,11 @@ This is a temporary script file.
 """
 import os
 import pandas
-from lxml import etree
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import SubElement
+
+from xml.dom import minidom
 #폴더 설정
 Current_Dir = os.getcwd() #현재폴더로 설정함
 file_list = os.listdir(Current_Dir)
@@ -61,18 +65,58 @@ df = pandas.read_csv(NFD_list[0], skiprows=5, names=['Index', 'X', 'Y', 'Z', 'Ex
 CSV_data = df.set_index("Index")
 CSV_data['Y']
 EmissionType_list=["Ex", "Ey", "Ez", "Hx", "Hy", "Hz"]
-root = etree.Element("EmissionScan")
-NFS_ver = etree.Element("Nfs_ver")
-NFS_ver.text = "1.0"
-File_name = etree.Element("Filename")
-File_name.text = AND_fileName+EmissionType_list[0]+".xml"
-File_ver = etree.Element("File_ver")
-File_ver.text = "1"
-ProbeType1 = etree.Element("Probe")
-ProbeType2 = etree.SubElement("Field")
-ProbeType2.text = EmissionType_list[0]
-Data_elem = etree.Element("Data")
-Coordi_elem = etree.SubElement("Coordinates")
-Coordi_elem.text="xyz"
-Freq_elem = etree.SubElement("Frequencies")
-Freq_elem.tex
+
+## xml write
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+
+root = Element('EmissionScan')
+for key in phonelist.keys():
+    xml_Nfs_ver = SubElement(root, 'Nfs_ver')
+    xml_Nfs_ver.text = '1.0'
+    xml_Filename = SubElement(root, 'Filename')
+    xml_Filename.text = 'Ex.xml'
+    xml_Filename = SubElement(root, 'File_ver')
+    xml_Filename.text = '1'
+    xml_Probe = SubElement(root, 'Probe')
+    xml_Probe_Field = SubElement(xml_Probe, 'Field')
+    xml_Probe_Field.text = 'Ex'
+    xml_Data = SubElement(root, 'Data')
+    xml_Data_Coordinates = SubElement(xml_Data, 'Coordinates')
+    xml_Data_Coordinates.text = 'xyz'
+    xml_Data_Frequencies = SubElement(xml_Data, 'Frequencies')
+    xml_Data_Frequencies_List = SubElement(xml_Data_Frequencies, 'List')
+    xml_Data_Frequencies_List.text = ##Freq List
+    xml_Data_Measurement = SubElement(xml_Data, 'Measurement')
+    xml_Data_Measurement_Format = SubElement(xml_Data_Measurement, 'Format')
+    xml_Data_Measurement_Format.text = 'ri' ## Format
+    xml_Data_Measurement_Data_files = SubElement(xml_Data_Measurement, 'Data_files')
+    xml_Data_Measurement_Data_files.text = 'Ex.dat' ## Format
+
+output_file = open( 'Emission.xml', 'w' )
+output_file.write( '<?xml version="1.0" encoding="UTF-8"?>' )
+output_file.write( prettify(root))
+output_file.close()
+
+#
+# root = etree.Element("EmissionScan")
+# NFS_ver = etree.Element("Nfs_ver")
+# NFS_ver.text = "1.0"
+# File_name = etree.Element("Filename")
+# File_name.text = AND_fileName+EmissionType_list[0]+".xml"
+# File_ver = etree.Element("File_ver")
+# File_ver.text = "1"
+# ProbeType1 = etree.Element("Probe")
+# ProbeType2 = etree.SubElement("Field")
+# ProbeType2.text = EmissionType_list[0]
+# Data_elem = etree.Element("Data")
+# Coordi_elem = etree.SubElement("Coordinates")
+# Coordi_elem.text="xyz"
+# Freq_elem = etree.SubElement("Frequencies")
+# Freq_elem.tex
